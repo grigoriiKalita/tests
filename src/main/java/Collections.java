f7d56e49f0f67c1fd1b178665c8cb1d94140b793
gkalita <grigorii.kalita@t-systems.com>
2021-03-03 21:52:46 +0300

    fixes

--- src/main/java/Collections.java
+++ src/main/java/Collections.java
@@ -5,28 +5,70 @@ import java.util.Set;
 
 public class Collections {
 
-    List<String> formNewListWithoutRepetitions(List<String> list1, List<String> list2) {
-        List<String> resultArray = new ArrayList<String>();
+    List<User> formNewListWithoutRepetitions(List<User> list1, List<User> list2) {
 
-        for (String valueOfList1 : list1) {
-            boolean isDuplicate = false;
-            for (String valueOfList2 : list2) {
-                if (valueOfList1.equals(valueOfList2)) {
-                    isDuplicate = true;
-                    break;
+        List<User> resultArray = new ArrayList<User>();
+        resultArray.addAll(list1);
+
+        for (int i = 0; i < resultArray.size(); i++) {
+            for (int j = i+1; j < resultArray.size(); i++) {
+                if (resultArray.get(i).equals(resultArray.get(j))) {
+                    resultArray.remove(resultArray.get(j));
                 }
             }
-            if (isDuplicate) {
-                resultArray.add(valueOfList1);
-            }
         }
+        resultArray.retainAll(list2);
+
         return resultArray;
     }
 
-    Set<String> formSetFromLists(List<String> list1, List<String> list2) {
-        Set<String> resultSet = new HashSet<>(list1);
-        resultSet.retainAll(list2);
+    Set<User> formSetFromLists(Set<User> set1, Set<User> set2) {
+
+        Set<User> resultSet = new HashSet<User>(set1);
+
+        resultSet.addAll(set1);
+        resultSet.retainAll(set2);
         return resultSet;
     }
+
+    //        // 1 //
+//
+//        List<String> array = new ArrayList<String>();
+//        array.add("first");
+//        array.add("first");
+//        array.add("banana");
+//        for (int i = 0; i <= array.size()-1; i++) {
+//            System.out.println(i + " : " + array.get(i));
+//        }
+//
+//        // 2 //
+//
+//        Set<String> set = new HashSet<>();
+//        set.add("SetFirst");
+//        set.add("SetFirst");
+//        set.add("JOJO");
+//        for (String value : set) {
+//            System.out.println(value);
+//        }
+//
+//        // 3 //
+//
+//        Map<String, ArrayList<String>> map = new HashMap<>();
+//        ArrayList<String> mapValueArray = new ArrayList<>();
+//        mapValueArray.addAll(array);
+//
+//        map.put("Dat's da key", mapValueArray);
+//
+//        for (ArrayList<String> value : map.values()) {
+//            System.out.println(value);
+//        }
+//
+//        for (String key : map.keySet()) {
+//            System.out.println(key);
+//        }
+//
+//        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,35 +3,58 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        Collections collections = new Collections();
+
+        Set<User> user1 = new HashSet<>();
+        Set<User> user2 = new HashSet<>();
+
+        User bob = new User("Bob", 1);
+        User bob2 = new User("Bob", 1);
+        User mark = new User("Bob", 1);
+        User mark2 = new User("Mark", 1);
+
+        user1.add(bob);
+        user1.add(bob2);
+
+        user2.add(mark);
+        user2.add(mark2);
+
         // 1 //
 
-        List<String> array = new ArrayList<String>();
-        array.add("first");
-        array.add("first");
-        array.add("banana");
+        List<User> array = new ArrayList<User>();
+        array.add(bob);
+        array.add(bob2);
+        array.add(mark);
+        array.add(mark2);
         for (int i = 0; i <= array.size()-1; i++) {
             System.out.println(i + " : " + array.get(i));
         }
 
         // 2 //
 
-        Set<String> set = new HashSet<>();
-        set.add("SetFirst");
-        set.add("SetFirst");
-        set.add("JOJO");
-        for (String value : set) {
+        Set<User> set = new HashSet<>();
+        set.add(bob);
+        set.add(bob2);
+        set.add(mark);
+        set.add(mark2);
+        for (User value : set) {
             System.out.println(value);
         }
 
         // 3 //
 
-        Map<String, ArrayList<String>> map = new HashMap<>();
-        ArrayList<String> mapValueArray = new ArrayList<>();
+
+        Map<String, ArrayList<User>> map = new HashMap<>();
+        ArrayList<User> mapValueArray = new ArrayList<>();
         mapValueArray.addAll(array);
 
         map.put("Dat's da key", mapValueArray);
+        map.put("Dat's da key", mapValueArray);
+        map.put("Dat's da key2", mapValueArray);
 
-        for (ArrayList<String> value : map.values()) {
+
+        for (ArrayList<User> value : map.values()) {
             System.out.println(value);
         }
 
@@ -39,7 +62,7 @@ public class Main {
             System.out.println(key);
         }
 
-        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+        for (Map.Entry<String, ArrayList<User>> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
